19245ce622c0edd7a66f02eb7619b2aef98bc948
Gowtham <gowreact@gmail.com>
2026-05-27 12:01:05 +0530

    frontend app developed with skeleton view

---
+++ src/layout/footer/index.tsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+// @desc - Shared AppFooter layout component
+const AppFooter = () => {
+  return (
+    <>
+      <div className="AppFooter-container">
+        <p>Copyright © 2026 gowtham. All rights reserved.</p>
+      </div>
+    </>
+  );
+};
+
+export default AppFooter;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,18 @@
+import "./style.css";
+
+// @desc -  home component
+const Home = () => {
+  return (
+    <>
+      <div className="Home-container">
+        <div className="Home-section1"></div>
+        <div className="Home-section2"></div>
+        <div className="Home-section3"></div>
+        <div className="Home-section4"></div>
+        <div className="Home-section5"></div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
