6da49c6e772e77a0952f914a760f1b8759dc2fa8
Gowtham <gowreact@gmail.com>
2026-06-05 16:54:41 +0530

    Welcome text removed from Home section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,9 +6,7 @@ const Home = () => {
     <>
       <div className="Home-container">
         <div className="Home-section1"></div>
-        <div className="Home-section2">
-          <p>Welcome to the KT session</p>
-        </div>
+        <div className="Home-section2"></div>
         <div className="Home-section3"></div>
         <div className="Home-section4"></div>
         <div className="Home-section5"></div>
